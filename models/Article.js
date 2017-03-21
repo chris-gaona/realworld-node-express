93d9193411389cbbf2d5edbd936c34531433aa68
Chris Gaona <thebeloved88@sbcglobal.net>
2017-03-21 09:41:30 -0700

    Fixes slug bug found in Article model

--- models/Article.js
+++ models/Article.js
@@ -7,12 +7,10 @@ var User = mongoose.model('User');
 var ArticleSchema = new mongoose.Schema({
     slug: {
         type: String,
-        lowercase: true
-    },
-    title: {
-        type: String,
+        lowercase: true,
         unique: true
     },
+    title: String,
     description: String,
     body: String,
     favoritesCount: {
@@ -37,13 +35,14 @@ ArticleSchema.plugin(uniqueValidator, { message: 'is already taken' });
 
 // method used to create the unqiue url slug
 ArticleSchema.methods.slugify = function () {
-    // 36 possible characters in a string 6 characters long puts the odds of a title collision at 1 in over 2.17 billion (36^6), and that's assuming that the article title is exactly the same as an existing article
-    // Math.pow() function returns the base to the exponent power, that is, base exponent
-    // Math.pow(base, exponent)
-    // Math.random() function returns a floating-point, pseudo-random number in the range [0, 1) that is, from 0 (inclusive) up to but not including 1 (exclusive)
-    // numObj.toString([radix])
-    // For radixes above 10, the letters of the alphabet indicate numerals greater than 9
-    this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
+    // // 36 possible characters in a string 6 characters long puts the odds of a title collision at 1 in over 2.17 billion (36^6), and that's assuming that the article title is exactly the same as an existing article
+    // // Math.pow() function returns the base to the exponent power, that is, base exponent
+    // // Math.pow(base, exponent)
+    // // Math.random() function returns a floating-point, pseudo-random number in the range [0, 1) that is, from 0 (inclusive) up to but not including 1 (exclusive)
+    // // numObj.toString([radix])
+    // // For radixes above 10, the letters of the alphabet indicate numerals greater than 9
+    // this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
+    this.slug = slug(this.title);
 };
 
 // mongoose middleware pre mongoose validation to create the slug
